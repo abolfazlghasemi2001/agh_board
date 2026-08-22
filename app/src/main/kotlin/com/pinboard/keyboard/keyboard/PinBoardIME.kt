791b6e57083857d6c91a9f5b2f2ecf47a6e892b3
abolfazlghasemi2001 <128874038+abolfazlghasemi2001@users.noreply.github.com>
2026-08-22 13:48:48 +0330

    Update PinBoardIME.kt

--- app/src/main/kotlin/com/pinboard/keyboard/keyboard/PinBoardIME.kt
+++ app/src/main/kotlin/com/pinboard/keyboard/keyboard/PinBoardIME.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.inputmethodservice.InputMethodService
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.ContextThemeWrapper
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
